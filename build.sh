4100d70448b9791b40b4c8802ac2350bb491d50d
bala9618 <d.alab231@gmail.com>
2024-08-12 22:31:44 +0530

    updated

--- build.sh
+++ build.sh
@@ -3,17 +3,17 @@
 C_N="ProjectNodejs"
 I_N="ProjectNodejs"
 
-sudo docker ps | grep "$C_N" > /dev/null
+sudo -s docker ps | grep "$C_N" > /dev/null
 if [ $? -eq 0 ];
 then
-sudo docker stop $C_N
-sudo docker rm $C_N
+sudo -s docker stop $C_N
+sudo -s docker rm $C_N
 fi
 
-sudo docker images | grep "$I_N" > /dev/null
+sudo -s docker images | grep "$I_N" > /dev/null
 if [ $? -eq 0];
 then
-sudo docker rmi $I_N
+sudo -s docker rmi $I_N
 fi
 
 docker build -t $I_N .
